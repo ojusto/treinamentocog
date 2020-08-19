f95acf0859430f08d7155b340408165b8033b195
ojusto <55556542+ojusto@users.noreply.github.com>
2020-08-19 10:28:35 -0300

    Initial commit

---
+++ hellowordl/src/webparts/helloCog/HelloCogWebPart.ts
@@ -0,0 +1,64 @@
+import { Version } from "@microsoft/sp-core-library";
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { escape } from "@microsoft/sp-lodash-subset";
+
+import styles from "./HelloCogWebPart.module.scss";
+import * as strings from "HelloCogWebPartStrings";
+
+export interface IHelloCogWebPartProps {
+  description: string;
+}
+
+export default class HelloCogWebPart extends BaseClientSideWebPart<
+  IHelloCogWebPartProps
+> {
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${styles.helloCog}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+            <div class="${styles.column}">
+              <span class="${styles.title}">Ola Mundo!</span>
+              <p class="${styles.subTitle}">Essa é nossa primeira webpart.</p>
+              <p class="${styles.description}">${escape(
+      this.properties.description
+    )}</p>
+              <a href="https://aka.ms/spfx" class="${styles.button}">
+                <span class="${styles.label}">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse("1.0");
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription,
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  }
+}
---
+++ hellowordl/src/webparts/helloCog/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHelloCogWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HelloCogWebPartStrings' {
+  const strings: IHelloCogWebPartStrings;
+  export = strings;
+}
